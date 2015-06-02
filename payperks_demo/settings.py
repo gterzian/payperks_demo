5227e5d285a4a632d70052ba39a3897df5b69451
Gregory <gregory.terzian@gmail.com>
2015-06-02 21:46:30 +0800

    heroku settings, first pass

--- payperks_demo/settings.py
+++ payperks_demo/settings.py
@@ -114,3 +114,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
     'PAGE_SIZE': 10
 }
+
+if os.environ['HOME'] == '/app':
+    #heroku settings
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
