89d342630db6a56583350d88ce31230463a49170
Gregory <gregory.terzian@gmail.com>
2015-06-02 21:59:43 +0800

    changing something just to deploy on heroku

--- payperks_demo/settings.py
+++ payperks_demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,7 +119,6 @@ REST_FRAMEWORK = {
 if os.environ['HOME'] == '/app':
     #heroku settings
     # Parse database configuration from $DATABASE_URL
-    import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
