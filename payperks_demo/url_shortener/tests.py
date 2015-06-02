9cd8c986fe2a93db1b001299d48861e216cd365a
Gregory <gregory.terzian@gmail.com>
2015-06-02 12:20:41 +0800

    better test names

--- payperks_demo/url_shortener/tests.py
+++ payperks_demo/url_shortener/tests.py
@@ -15,12 +15,12 @@ from payperks_demo.url_shortener.utils import short_string, LETTERS_AND_DIGITS
 
 class TestShortString(Exam, TestCase):
     
-    def test_short_string_returns_letters_of_lenght(self):
+    def test_short_string_returns_result_of_lenght(self):
         for i in range(10):
             result = short_string(length=i)
             self.assertEquals(len(result), i)
 
-    def test_short_string_returns_unique_4_letters(self):
+    def test_short_string_returns_unique_4_letters_and_digits(self):
         already_generated = []
         for i in range(100):
             result = short_string(already=already_generated)
