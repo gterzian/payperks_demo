53b1d83f965a0377c059c831853c4f7c34804083
Gregory <gregory.terzian@gmail.com>
2015-06-02 17:36:22 +0800

    basic angular structure

---
+++ assets/app/tests/unit/controllers/test_url_shortener_create_controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Controller: trackingScriptCtrl', function () {
+
+  
+  it('The tracking script should initialize to an empty string, script_generate to false', function () {
+    expect(true).to.be.true;
+    
+  });
+  
+});
